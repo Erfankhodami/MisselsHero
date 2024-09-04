f934767cea65682ed40ee5f23d4df044e467d86f
Erfankhodami <137073191+fatemesadegi1658@users.noreply.github.com>
2024-09-04 14:25:37 +0330

    particly system doesnt works and its now cancled!

--- Assets/Scripts/MisslesController.cs
+++ Assets/Scripts/MisslesController.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class MisslesController : MonoBehaviour
 {
     private Rigidbody2D MissleRB;
-
+    [SerializeField] private ParticleSystem ParticleSystemP;
+    [SerializeField] private GameObject MissleSpriteG;
     [SerializeField] private float SpeedF;
     // Start is called before the first frame update
     void Start()
     {
         MissleRB = GetComponent<Rigidbody2D>();
+        //spawn the partcile system of missle flames
+        ParticleSystem particleSystemP;
+        GameObject missleSpriteG;
+        particleSystemP=Instantiate(ParticleSystemP, transform.position, quaternion.Euler(transform.rotation.x+90,transform.rotation.y,transform.rotation.z));
+        missleSpriteG=Instantiate(MissleSpriteG, transform.position, transform.rotation);
+        particleSystemP.transform.parent = gameObject.transform;
+        missleSpriteG.transform.parent = gameObject.transform;
+        
+
         StartCoroutine(DestroyMissle());
     }
 
